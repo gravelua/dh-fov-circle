01314bb100cc856bfa238101f11cfb6327db2a37
gravelua <102436294+gravelua@users.noreply.github.com>
2022-03-27 19:28:48 +0100

    Create circleoff.lua

---
+++ circleoff.lua
@@ -0,0 +1,17 @@
+_G.FOVCIRCLE = false
+local circle = Drawingnew("Circle")
+circle.Transparency = 1
+circle.Thickness = 2
+circle.Color = Aiming.FOVColour
+circle.Filled = false
+function Aiming.UpdateFOV()
+    if (circle) then
+        circle.Visible = Aiming.ShowFOV
+        circle.Radius = (Aiming.FOV * 3)
+        circle.Position = Vector2new(Mouse.X, Mouse.Y + GetGuiInset(GuiService).Y)
+        circle.NumSides = Aiming.FOVSides
+        circle.Color = Aiming.FOVColour
+
+        return circle
+    end
+end
